9d422dd9bcb4d7c733041776e8a4ebf1d30a72c8
ghidtm44 <todd.ghidaleson@me.com>
2024-11-08 10:23:13 -0500

    Update supabase.ts

--- src/lib/supabase.ts
+++ src/lib/supabase.ts
@@ -3,4 +3,9 @@ import { createClient } from '@supabase/supabase-js';
 const supabaseUrl = 'https://kheqhiyrvkwtuzxeiqre.supabase.co';
 const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImtoZXFoaXlydmt3dHV6eGVpcXJlIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MzEwNzUzNDgsImV4cCI6MjA0NjY1MTM0OH0.AKjog4bOwDQDseDaiucWxeeGN4AUg53LmgrKZ34fTKU';
 
-export const supabase = createClient(supabaseUrl, supabaseKey);
+export const supabase = createClient(supabaseUrl, supabaseKey, {
+  auth: {
+    autoRefreshToken: true,
+    persistSession: true,
+  },
+});
